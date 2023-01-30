94bd04ccff1df4e6b15b19daa3bff86079305051
shao <116676793+shaowastaken@users.noreply.github.com>
2023-01-30 17:46:07 +0100

    1.0

---
+++ scripts/myscript.js
@@ -0,0 +1,41 @@
+    let track1 = document.getElementById('aleNo');
+    function playAleNo(){
+    let audio = new Audio("/assets/suoni/alenooooooooo.mp3");
+    audio.play();
+    }
+    play.addEventListener("click", playMusic);
+
+    let track2 = document.getElementById('appunto');
+    function playAppunto(){
+    let audio = new Audio("/assets/suoni/appunto.mp3");
+    audio.play();
+    }
+    play.addEventListener("click", playMusic);    
+
+    let track3 = document.getElementById('gioiagiant');
+    function playGioiagiant(){
+    let audio = new Audio("/assets/suoni/gioiagiant.mp3");
+    audio.play();
+    }
+    play.addEventListener("click", playMusic);    
+
+    let track4 = document.getElementById('leimibullizza');
+    function playLeimibullizza(){
+    let audio = new Audio("/assets/suoni/leimibullizza.mp3");
+    audio.play();
+    }
+    play.addEventListener("click", playMusic); 
+
+    let track5 = document.getElementById('lodailsolemattia');
+    function playLodailsolemattia(){
+    let audio = new Audio("/assets/suoni/lodailsolemattia.mp3");
+    audio.play();
+    }
+    play.addEventListener("click", playMusic); 
+
+    let track6 = document.getElementById('disastro');
+    function playDisastro(){
+    let audio = new Audio("/assets/suoni/disastro.mp3");
+    audio.play();
+    }
+    play.addEventListener("click", playMusic); 
